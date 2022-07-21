b8c917e1b61ac8db078833f5596bf67b1eb0eb95
Emilio <emiliothomasking@gmail.com>
2022-07-21 13:18:28 -0700

    added req.param and req.query

--- server.js
+++ server.js
@@ -44,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
             if (req.query) {
             results = filterByQuery(req.query, results);
             }
         res.json(results);
+    });
+
+    app.get('/api/animals/:id', (req, res) => {
+      const result = findById(req.params.id, animals);
+      if (result) {
+        res.json(result);
+      } else {
+        res.sendStatus(404);
+      }
     });
 
 app.listen(PORT, () => {
