7e73e50b057b07dd0e1f2964212d8abf3e312d7c
Emili0 <emiliothomasking@gmail.com>
2022-08-31 22:09:29 -0700

    fix:

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get(':1', (req, res) => {
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
