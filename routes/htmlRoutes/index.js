61cc07cb39610e80657a20aad2682ebd77270121
Emili0 <emiliothomasking@gmail.com>
2022-08-31 22:21:06 -0700

    almost done fixing

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,19 +2,19 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../Public/index.html'));
 });
 
 router.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/animals.html'));
+  res.sendFile(path.join(__dirname, '../../Public/animals.html'));
 });
 
 router.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+  res.sendFile(path.join(__dirname, '../../Public/zookeepers.html'));
 });
 
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../Public/index.html'));
 });
 
 module.exports = router;
